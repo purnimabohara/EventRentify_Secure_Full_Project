e77fb0ab8b9d8042e6228b9483e599d605fae866
purnimabohara <purnimabohara6@gmail.com>
2024-08-10 21:37:24 +0545

    nodemailer implemenetation

---
+++ routes/logRoute.js
@@ -0,0 +1,14 @@
+// const express = require("express");
+// const router = express.Router();
+// const logController = require("../controllers/logController.js");
+
+// // Route to get all logs
+// router.get("/logs", logController.getAllLogs);
+
+// // Route to get a specific log by ID
+// router.get("/logs/:id", logController.getLogById);
+
+// // Route to delete a specific log by ID
+// router.delete("/logs/:id", logController.deleteLogById);
+
+// module.exports = router;
